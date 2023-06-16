c2beb84b3db4fdf30739a5cf2fe57d7610dab317
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-16 14:04:43 -0700

    Created HelperConfig for sepolia and anvil mocks

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+
+pragma solidity ^0.8.18;
+
+import {Script} from "@forge-std/src/Script.sol";
+
+contract HelperConfig {
+    // If we are on a local anvil, we deploy mocks
+    // Otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig; 
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory){
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns(NetworkConfig memory){
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return anvilConfig;
+    }
+}
