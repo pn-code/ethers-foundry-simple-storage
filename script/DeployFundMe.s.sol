54dd8701cca675f896202edcadc451e83f8bc1ca
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-16 14:05:20 -0700

    Substituted hard-coded address for HelperConfig

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.18;
 
 import {Script} from "@forge-std/src/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe){
+        // Anything before startBroadcast is not a "real" tx (this does not spend gas)
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
