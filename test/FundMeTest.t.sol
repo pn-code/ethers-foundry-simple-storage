bfb5ac98da4a1e0ca77fc04d1112fac6f7750ea1
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-19 09:24:02 -0700

    Added tests for fund function

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,10 +9,15 @@ contract FundMeTest is Test {
     // Initiate var
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     // Provide value to var
     function setUp() external {
         DeployFundMe deployedFundMe = new DeployFundMe();
         fundMe = deployedFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     // Check minimum USD
@@ -31,5 +36,27 @@ contract FundMeTest is Test {
     // Checks price feed version
     function testPriceFeedVersionIsAccurate() public {
         assertEq(fundMe.getVersion(), 4);
+    }
+
+    // Checks to see if fund fails/reverts if not enough ETH
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // next line should revert
+        fundMe.fund();
+    }
+
+    // Fund should work when funded
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    // Fund function should add funder to arrays of funders
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        address funder = fundMe.getFunder(0);
+        assertEq(USER, funder);
     }
 }
